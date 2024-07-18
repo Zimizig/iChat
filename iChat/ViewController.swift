30bc52c4f0bea8126bd9b3d6152c3489f9914804
Zimizig <ramazanow9999@gmail.com>
2024-07-18 15:40:08 +0500

    Template for an application with a deleted Storyboard and a customized Canvas

--- iChat/ViewController.swift
+++ iChat/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
 }
 
+struct viewControllerProvider: PreviewProvider {
+    
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let viewController = ViewController()
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<viewControllerProvider.ContainerView>) -> ViewController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+            
+        }
+    }
+}
